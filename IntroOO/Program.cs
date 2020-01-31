264755385ff6fb2e2bf8f21b6730391293b52c31
Julius Leblanc <1974350@cstj.qc.ca>
2020-01-31 17:00:37 -0500

    commentaire

---
+++ IntroOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroOO
+{
+    class Program
+    {
+        // Le paramètre "args" (tableau de chaines de caractères) va servir à recevoir des arguments lors du lancement du programme.
+        // Exemples à venir plus tard.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
